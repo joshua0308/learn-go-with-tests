3847b784e1e4f60422e0c117afbfada4dc201ec7
Josh Kim <josh.kim@resolver.com>
2023-03-10 10:12:56 -0500

    done: hello world

--- hello.go
+++ hello.go
@@ -3,9 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
 
-func Hello(name string) string {
-	return "Hello, world"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
