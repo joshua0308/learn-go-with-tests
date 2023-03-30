6d7204712c00d635cd4896bc2a7cef20fdd1f610
Josh Kim <josh.kim@resolver.com>
2023-03-30 01:14:13 -0400

    reflection: refactor walk fn

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -60,6 +60,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
 	}
 
 	for _, test := range cases {
@@ -79,15 +87,17 @@ func TestWalk(t *testing.T) {
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
 		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
