f62acbc588730a9233771656f09b64f57216b848
Josh Kim <josh.kim@resolver.com>
2023-04-21 23:32:31 -0400

    numeral: roman to arabic

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -5,33 +5,44 @@ import (
 	"testing"
 )
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Arabic int
-		Roman  string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{9, "IX"},
-		{10, "X"},
-		{14, "XIV"},
-		{18, "XVIII"},
-		{20, "XX"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{47, "XLVII"},
-		{49, "XLIX"},
-		{50, "L"},
-	}
+var cases = []struct {
+	Arabic int
+	Roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{9, "IX"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+}
 
+func TestRomanNumerals(t *testing.T) {
 	for _, test := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
 			if got != test.Roman {
 				t.Errorf("got %q, want %q", got, test.Roman)
+			}
+		})
+	}
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
 	}
