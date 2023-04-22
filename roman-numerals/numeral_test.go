348e51283ba3bde8f7a30f22746761631757a064
Josh Kim <josh.kim@resolver.com>
2023-04-21 23:48:48 -0400

    numeral: done

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -54,12 +54,15 @@ func TestPropertiesOfConversion(t *testing.T) {
 		if arabic > 3999 {
 			return true
 		}
+		t.Log("testing", arabic)
 		roman := ConvertToRoman(arabic)
 		fromRoman := ConvertToArabic(roman)
 		return fromRoman == arabic
 	}
 
-	if err := quick.Check(assertion, nil); err != nil {
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+	}); err != nil {
 		t.Error("failed checks", err)
 	}
 }
